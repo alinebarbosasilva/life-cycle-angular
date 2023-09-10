7ed186bddaf2ead89d8840c02eecfa6a01914ee1
alinebarbosasilva <alinnee_barbosa@outlook.com>
2023-09-09 23:47:27 -0300

    Criado novo repositório para praticar o ciclo de vida do angular

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {ListaDeCompraService} from "../../service/lista-de-compra.service";
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  valorItem!: string;
+
+  constructor(
+    private listaService: ListaDeCompraService
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  adicionarItem() {
+    this.listaService.adicionarItemNaLista(this.valorItem);
+    this.limparCampo();
+  }
+  limparCampo(){
+    this.valorItem = '';
+  }
+}
---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {Item} from "../../interfaces/iItem";
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges {
+  @Input() item!: Item;
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('OnInit')
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChange')
+  }
+
+}
